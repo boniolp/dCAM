fbb5ea9ce31580222c89e3dfcd1f91dcfcb8089a
boniolp <boniol.paul@gmail.com>
2022-08-27 14:18:17 +0200

    new results

---
+++ experiments/execution_time/scripts_training/auto_script/submit_cinception_synth_ShapesAll_type1_nbdim_20_epoch_dim.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+#SBATCH --job-name=synth_cinception_synth_ShapesAll_type1_nbdim_20_epoch_exp_dim        # name of job
+#SBATCH --ntasks=1                   # total number of processes (= number of GPUs here)
+#SBATCH --gres=gpu:1                 # number of GPUs (1/4 of GPUs)
+#SBATCH --cpus-per-task=10           # number of cores per task (1/4 of the 4-GPUs node)
+#SBATCH --hint=nomultithread         # hyperthreading is deactivated
+#SBATCH --time=20:00:00              # maximum execution time requested (HH:MM:SS)
+#SBATCH --output=auto_script/output_log/synth_cinception_synth_ShapesAll_type1_nbdim_20_epoch_exp_dim.out    # name of output file
+#SBATCH --error=auto_script/output_log/synth_cinception_synth_ShapesAll_type1_nbdim_20_epoch_exp_dim.out     # name of error file (here, in common with the output file)
+
+
+# cleans out the modules loaded in interactive and inherited by default 
+module purge
+ 
+# loading of modules
+module load pytorch-gpu/py3/1.7.0
+ 
+# echo of launched commands
+set -x
+
+# code execution
+python -u script_exp_dataset_dim.py cinception c ../../../data/synthetic/synth_ShapesAll_type1_nbdim_20.pickle 10 5 4 0.8
---
+++ experiments/execution_time/scripts_training/script_exp_gpu_parallel_dim.py
@@ -0,0 +1,81 @@
+import os
+import sys
+from submit_generic import head
+
+
+if __name__ == '__main__':
+        
+
+        dataset_names = [
+                'synth_ShapesAll_type1_nbdim_10.pickle',
+                'synth_ShapesAll_type1_nbdim_20.pickle',
+                'synth_ShapesAll_type1_nbdim_40.pickle',
+                'synth_ShapesAll_type1_nbdim_60.pickle',
+                'synth_ShapesAll_type1_nbdim_100.pickle',
+        ]
+        
+        model_names = [
+                ('mtex','c'),
+                ('cnn','baseline'),
+                ('inception','baseline'),
+                ('resnet','baseline'),
+                ('cresnet','c'),
+                ('ccnn','c'),
+                ('cinception','c'),
+                ('dcnn','d'),
+                ('dresnet','d'),
+                ('dinception','d'),
+        ]
+
+        try:
+                with open("submit_all_epoch_exp_dim.sh",'w') as f:
+                        f.write("#!/bin/bash \n")
+                count = 0
+                for name in dataset_names: 
+                        
+                        dataset_name = '../../../data/synthetic/{}'.format(name)
+                        
+                        device='cuda'
+                        
+                        # Parameter in papers and for the results depicted in the paper
+                        #parameters = {
+                        #       'train_test_r': 0.80,
+                        #       'batch_size': 32,
+                        #       'nb_epoch': 1000,
+                        #       'nb_repeat_iteration': 10
+                        #}
+
+                        # Parameter to test the code
+                        parameters = {
+                                'train_test_r': 0.80,
+                                'batch_size': 4,
+                                'nb_epoch': 10,
+                                'nb_repeat_iteration': 5
+                        }
+
+
+
+                        for model_name,type_input in model_names:
+                                submit_name = 'auto_script/submit_{}_{}_epoch_dim.sh'.format(model_name,name.strip('.pickle'))
+                                
+                                with open("submit_all_epoch_exp_dim.sh",'a') as f:
+                                    f.write("sbatch {}\n".format(submit_name))
+                                    #f.write("sleep 5 \n")
+                                count += 1
+                                with open(submit_name,'w') as f:
+                                        script_name = 'script_exp_dataset_dim.py {} {} {} {} {} {} {}'.format(model_name,type_input,dataset_name,parameters['nb_epoch'],parameters['nb_repeat_iteration'], parameters['batch_size'], parameters['train_test_r'])
+                                        name_job = 'synth_{}_{}_epoch_exp_dim'.format(model_name,name.strip('.pickle'))
+                                        f.write(head.format(name_job,name_job,name_job,script_name))
+        except KeyboardInterrupt:
+                print('interuption...')
+
+
+
+                        
+
+
+
+
+
+
+
